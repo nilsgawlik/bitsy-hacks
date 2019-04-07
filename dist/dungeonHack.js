af00af830dc795d57fd93a98889cf2c127d16f36
Nils Gawlik <nilsgawlik@gmx.de>
2019-04-07 11:04:17 +0200

    Add basic generation and room transition hook

--- dist/dungeonHack.js
+++ dist/dungeonHack.js
@@ -93,13 +93,14 @@ HOW TO USE:
   https://github.com/seleb/bitsy-hacks/wiki/Coding-with-kitsy
 */
 
-// Ex: before('load_game', function run() { alert('Loading!'); });
-//     before('show_text', function run(text) { return text.toUpperCase(); });
-//     before('show_text', function run(text, done) { done(text.toUpperCase()); });
-function before(targetFuncName, beforeFn) {
+
+// Ex: inject(/(names.sprite.set\( name, id \);)/, '$1console.dir(names)');
+function inject$1(searchRegex, replaceString) {
 	var kitsy = kitsyInit();
-	kitsy.queuedBeforeScripts[targetFuncName] = kitsy.queuedBeforeScripts[targetFuncName] || [];
-	kitsy.queuedBeforeScripts[targetFuncName].push(beforeFn);
+	kitsy.queuedInjectScripts.push({
+		searchRegex: searchRegex,
+		replaceString: replaceString
+	});
 }
 
 // Ex: after('load_game', function run() { alert('Loaded!'); });
@@ -217,30 +218,101 @@ function _reinitEngine() {
 
 
 
-console.log("Dungeon hack is active!");
-
 var dungeon = {
     map: [],
     curPos: {x: 0, y: 0},
-    prevRoom: 0,
+    prevRoom: -7,
 };
 
-before("load_game", function() {
-    let w = 14;
-    let h = 6;
-    for(var x = 0; x < w; x++)
-    for(var y = 0; y < h; y++) {
-        dungeon.map[x][y] = Math.random() < 0.5? "R" : ".";
-    }
-});
+// room tiles
+var specialTiles = {
+    room: null,
+    stairs: null,
+    boss: null,
+    empty: "0",
+};
 
 after("load_game", function() {
-    dungeon.prevRoom = curRoom;
+
+    Object.keys(tile).forEach(function(key,index) {
+        let t = tile[key];
+
+        if(t.name) {
+            if(t.name == "mapRoom") {
+                specialTiles.room = t.id;
+            } else if(t.name == "mapStairs") {
+                specialTiles.stairs = t.id;
+            } else if(t.name == "mapBoss") {
+                specialTiles.boss = t.id;
+            }
+        }
+    });
+
+
+    
 });
 
-before("update", function() {
+inject$1(/curRoom \= ext.dest.room;/g, "curRoom = ext.dest.room;\nwindow.onRoomChange();");
+window.onRoomChange = function() {
+    generateDungeonMap();
+};
 
-});
+function generateDungeonMap() {
+    console.log("GENERATING DUNGEON...");
+
+    // generate empty world map
+    let w = 14;
+    let h = 7;
+    let inside = (x, y) => (x >= 0 && x < w && y >= 0 && y < h);
+
+    dungeon.map = new Array(h);
+    for(let x = 0; x < w; x++) {
+        dungeon.map[x] = new Array(h);
+        for(let y = 0; y < h; y++) {
+            dungeon.map[x][y] = specialTiles.empty;
+        }
+    }
+
+    // populate internal world map with path
+    let curPos = {x: 7, y: 3};
+    let steps = 33;
+    dungeon.map[curPos.x][curPos.y] = specialTiles.stairs;
+    for(let i = 0; i < steps; i++) {
+        // generator performs a random step
+        let prevPos = {x: curPos.x, y: curPos.y};
+        let d = Math.floor(Math.random() * 4) * Math.PI * 0.5;
+        let interPos = {
+            x: curPos.x + Math.round(Math.cos(d)),
+            y: curPos.y + Math.round(Math.sin(d)),
+        };
+        curPos.x = curPos.x + Math.round(Math.cos(d) * 2);
+        curPos.y = curPos.y + Math.round(Math.sin(d) * 2);
+
+        // check for collision
+        if(!inside(curPos.x, curPos.y) || dungeon.map[curPos.x][curPos.y] != specialTiles.empty) {
+            curPos = prevPos;
+        } else {
+            // build path
+            dungeon.map[interPos.x][interPos.y] = specialTiles.room;
+            dungeon.map[curPos.x][curPos.y] = specialTiles.room;
+        }
+
+        // check if last step, place boss room
+        if(i == steps - 1) {
+            dungeon.map[curPos.x][curPos.y] = specialTiles.boss;
+        }
+    }
+
+    // "render" world map to the room
+    let tilemap = room[curRoom].tilemap;
+    let mapOrigin = {x: 1, y: 8};
+
+    for(let x = 0; x < w; x++) 
+    for(let y = 0; y < h; y++) {
+        // y,x order not x,y !!!
+        tilemap[mapOrigin.y + y][mapOrigin.x + x] = dungeon.map[x][y];
+    }
+}
 
 exports.hackOptions = hackOptions;
 
--- src/dungeonHack.js
+++ src/dungeonHack.js
@@ -14,33 +14,105 @@ sdaf fdfa
 import bitsy from "bitsy";
 import {
     before,
-	after
+	after,
+    inject
 } from "./helpers/kitsy-script-toolkit";
 
 export var hackOptions = {
 };
 
-console.log("Dungeon hack is active!")
-
 var dungeon = {
     map: [],
     curPos: {x: 0, y: 0},
-    prevRoom: 0,
+    prevRoom: -7,
 }
 
-before("load_game", function() {
-    let w = 14;
-    let h = 6;
-    for(var x = 0; x < w; x++)
-    for(var y = 0; y < h; y++) {
-        dungeon.map[x][y] = Math.random() < 0.5? "R" : ".";
-    }
-});
+// room tiles
+var specialTiles = {
+    room: null,
+    stairs: null,
+    boss: null,
+    empty: "0",
+}
 
 after("load_game", function() {
-    dungeon.prevRoom = curRoom;
+
+    Object.keys(tile).forEach(function(key,index) {
+        let t = tile[key];
+
+        if(t.name) {
+            if(t.name == "mapRoom") {
+                specialTiles.room = t.id;
+            } else if(t.name == "mapStairs") {
+                specialTiles.stairs = t.id;
+            } else if(t.name == "mapBoss") {
+                specialTiles.boss = t.id;
+            }
+        }
+    });
+
+
+    
 });
 
-before("update", function() {
+inject(/curRoom \= ext.dest.room;/g, "curRoom = ext.dest.room;\nwindow.onRoomChange();")
+window.onRoomChange = function() {
+    generateDungeonMap();
+}
 
-});
+function generateDungeonMap() {
+    console.log("GENERATING DUNGEON...");
+
+    // generate empty world map
+    let w = 14;
+    let h = 7;
+    let inside = (x, y) => (x >= 0 && x < w && y >= 0 && y < h);
+
+    dungeon.map = new Array(h);
+    for(let x = 0; x < w; x++) {
+        dungeon.map[x] = new Array(h);
+        for(let y = 0; y < h; y++) {
+            dungeon.map[x][y] = specialTiles.empty;
+        }
+    }
+
+    // populate internal world map with path
+    let curPos = {x: 7, y: 3};
+    let steps = 33;
+    dungeon.map[curPos.x][curPos.y] = specialTiles.stairs;
+    for(let i = 0; i < steps; i++) {
+        // generator performs a random step
+        let prevPos = {x: curPos.x, y: curPos.y};
+        let d = Math.floor(Math.random() * 4) * Math.PI * 0.5;
+        let interPos = {
+            x: curPos.x + Math.round(Math.cos(d)),
+            y: curPos.y + Math.round(Math.sin(d)),
+        };
+        curPos.x = curPos.x + Math.round(Math.cos(d) * 2);
+        curPos.y = curPos.y + Math.round(Math.sin(d) * 2);
+
+        // check for collision
+        if(!inside(curPos.x, curPos.y) || dungeon.map[curPos.x][curPos.y] != specialTiles.empty) {
+            curPos = prevPos;
+        } else {
+            // build path
+            dungeon.map[interPos.x][interPos.y] = specialTiles.room;
+            dungeon.map[curPos.x][curPos.y] = specialTiles.room;
+        }
+
+        // check if last step, place boss room
+        if(i == steps - 1) {
+            dungeon.map[curPos.x][curPos.y] = specialTiles.boss;
+        }
+    }
+
+    // "render" world map to the room
+    let tilemap = room[curRoom].tilemap;
+    let mapOrigin = {x: 1, y: 8};
+
+    for(let x = 0; x < w; x++) 
+    for(let y = 0; y < h; y++) {
+        // y,x order not x,y !!!
+        tilemap[mapOrigin.y + y][mapOrigin.x + x] = dungeon.map[x][y];
+    }
+}
